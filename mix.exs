618af18f58865a83702fc92c32b82313cc257ba6
Andrew Vy <andrew@andrewvy.com>
2015-11-05 13:13:38 -0800

    Upgrade to 0.0.3 to test hot code upgrade

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sapphire.Mixfile do
 
   def project do
     [app: :sapphire,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
